c942ea1e1e733a7687da22c052e7dea45bcb2890
Maija <hello@maijagrudule.com>
2018-01-30 14:59:04 +0100

    [w3-003]User can post & see categories of posts

--- post.php
+++ post.php
@@ -10,13 +10,19 @@ if ($conn->connect_error) {
 }
 
 
-$sql = "SELECT post_id, post_title, post_content FROM Posts";
-$result = $conn->query($sql);
+$sql = "SELECT Posts.post_id, Posts.post_title, Posts.post_content , PostCategory.category_id, Categories.category_title
+FROM Posts
+JOIN PostCategory ON Posts.post_id=PostCategory.post_id
+JOIN Categories ON Categories.category_id=PostCategory.category_id";
 
+
+
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
+
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "post_id: " . $row["post_id"]. "<h1>" . $row["post_title"]. "</h1> <p> " . $row["post_content"]. "</p><br>";
+        echo "post_id: " . $row["post_id"]. "<h1>" . $row["post_title"]. "</h1> <h5> category "  . $row["category_title"] . "</h5> <p> " . $row["post_content"]. "</p><br>". $row["post_content"] . "<br> ";
     }
 } else {
     echo "0 results";
